d5190ebffcd0649c5a6d98347875b98073ccbc2b
RobesNo1 <andyrobey@me.com>
2025-05-16 14:44:22 +0100

    Update Start-DTSOSDCloud-LandingPage.ps1

--- Start-DTSOSDCloud-LandingPage.ps1
+++ Start-DTSOSDCloud-LandingPage.ps1
@@ -1,78 +1,31 @@
-# Override Global Variables
-
-#Variables to define the Windows OS / Edition to be applied during OSDCloud
-#$OSName = 'Windows 11 23H2 x64' < commented out 23/08/2024 as we may need W10 in niche cases
+# Set Windows OS variables
+$OSName = 'Windows 11 24H2 x64'
 $OSEdition = 'Enterprise'
 $OSActivation = 'Retail'
 $OSLanguage = 'en-gb'
 
-#Set OSDCloud Vars
+# Set OSDCloud global configuration
 $Global:MyOSDCloud = [ordered]@{
-    Restart = [bool]$False
-    RecoveryPartition = [bool]$true
-    OEMActivation = [bool]$True
-    WindowsUpdate = [bool]$true
-    WindowsUpdateDrivers = [bool]$true
-    WindowsDefenderUpdate = [bool]$true
-    SetTimeZone = [bool]$true
-    ClearDiskConfirm = [bool]$False
-    ShutdownSetupComplete = [bool]$false
-    SyncMSUpCatDriverUSB = [bool]$true
-    CheckSHA1 = [bool]$true
+    Restart = $false
+    RecoveryPartition = $true
+    OEMActivation = $true
+    WindowsUpdate = $true
+    WindowsUpdateDrivers = $true
+    WindowsDefenderUpdate = $true
+    SetTimeZone = $true
+    ClearDiskConfirm = $false
+    ShutdownSetupComplete = $false
+    SyncMSUpCatDriverUSB = $true
+    CheckSHA1 = $true
 }
 
-$banner = @"
-//////////////////////////////////////////////////
-//  ___  ____  ____   ____ _                 _  //
-// / _ \/ ___||  _ \ / ___| | ___  _   _  __| | //
-//| | | \___ \| | | | |   | |/ _ \| | | |/ _  | //
-//| |_| |___) | |_| | |___| | (_) | |_| | (_| | //
-// \___/|____/|____/ \____|_|\___/ \__,_|\__,_| //
-//                                              //
-//////////////////////////////////////////////////
-"@
-$loadingColor = "Yellow"
-Write-Host $banner -ForegroundColor $loadingColor
-
-$loadingMessage1 = "Created by Dave Segura (@SeguraOSD)"
-$loadingMessage2 = "You may now remove your USB key"
-$loadingMessage3 = "NOTE: OSDCloud has not been tested with Getac or Panasonic Tablets"
-$loadingColor = "White"
-$delay = 30  # Delay between each character in milliseconds
-
-# Function to simulate typing effect
-function Type-Write ($message, $color, $delay) {
-    foreach ($char in $message.ToCharArray()) {
-        Write-Host -NoNewline $char -ForegroundColor $color
-        Start-Sleep -Milliseconds $delay
-    }
-    Write-Host  # Move to the next line after the message is fully typed
-}
-
-# Type out the messages with a delay
+# Optional banner
 Write-Host ""
-Type-Write $loadingMessage1 $loadingColor $delay
-Start-Sleep -Seconds 1  # Optional pause between the lines
-Type-Write $loadingMessage2 $loadingColor $delay
-Start-Sleep -Seconds 1  # Optional pause between the lines
-$delay = 0 # Speeds up the final message about Field devices
-Type-Write $loadingMessage3 $loadingColor $delay
-write-host ""
-Write-Host "Version:: 1.1 (Auto Enterprise)" -ForegroundColor Yellow
-
+Write-Host "Starting OSDCloud for Windows 11 Enterprise..." -ForegroundColor Yellow
 Write-Host ""
-#Write-Host "===================== Main Menu =======================" -ForegroundColor Yellow
-#Write-Host "=======================================================" -ForegroundColor Yellow
-#Write-Host "1: Windows 11 | Enterprise   | En-gb" -ForegroundColor White
-#Write-Host "2: Windows 11 | Professional | Device Recycling Only" -ForegroundColor White
-#Write-Host "3: Exit`n" -ForegroundColor Red
 
-#$EngineerInput = Read-Host "Please make a selection"
+# Start the deployment
+Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage
 
-Write-Host  "Loading OSDCloud..." -ForegroundColor Green
-Write-Host ".................................................." -ForegroundColor Green
-
-# Auto-launch Enterprise build
-Start-OSDCloud -OSName "Windows 11 24H2 x64" -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage
-
+# Reboot when complete
 wpeutil reboot
