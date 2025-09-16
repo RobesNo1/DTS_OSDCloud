71be9f48a30d51c0510c706797ca633f0d7de338
RobesNo1 <andyrobey@me.com>
2025-09-16 08:19:12 +0100

    Update Start-DTSOSDCloud-LandingPageRes.ps1

--- Start-DTSOSDCloud-LandingPageRes.ps1
+++ Start-DTSOSDCloud-LandingPageRes.ps1
@@ -1,1 +1,31 @@
-Dummy
+# Set Windows OS variables
+$OSName = 'Windows 11 24H2 x64'
+$OSEdition = 'Pro'
+$OSActivation = 'Retail'
+$OSLanguage = 'en-gb'
+
+# Set OSDCloud global configuration
+$Global:MyOSDCloud = [ordered]@{
+    Restart = $false
+    RecoveryPartition = $true
+    OEMActivation = $true
+    WindowsUpdate = $true
+    WindowsUpdateDrivers = $true
+    WindowsDefenderUpdate = $true
+    SetTimeZone = $true
+    ClearDiskConfirm = $false
+    ShutdownSetupComplete = $false
+    SyncMSUpCatDriverUSB = $true
+    CheckSHA1 = $true
+}
+
+# Optional banner
+Write-Host ""
+Write-Host "Starting OSDCloud for Windows 11 Enterprise..." -ForegroundColor Yellow
+Write-Host ""
+
+# Start the deployment
+Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage
+
+# Reboot when complete
+wpeutil reboot
